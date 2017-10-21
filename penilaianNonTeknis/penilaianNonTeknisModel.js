0c0364c4034cc73d0b2964dffc864d9fd9a7ffa1
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-10-21 22:18:44 +0700

    no message

--- penilaianNonTeknis/penilaianNonTeknisModel.js
+++ penilaianNonTeknis/penilaianNonTeknisModel.js
@@ -20,13 +20,13 @@ let mong = require('mongoose');
 let penilaianNonTeknis = mong.Schema({
     KdPegawai: String,
 	KdPeserta:String,
-	Hari:String,
-	Materi:String,
+	Minggu:String,
+	TipePenilaian:String,
 	Nilai:Number,
 	CreatedBy:String,
-    CreatedDate:String,
-    ModifiedBy:String,
-    ModifiedDate:String
+	CreatedDate:String,
+	ModifiedBy:String,
+	ModifiedDate:String
 });
 
 let PenilaianNonTeknis = module.exports = mong.model("PenilaianNonTeknis", penilaianNonTeknis, "PenilaianNonTeknis");

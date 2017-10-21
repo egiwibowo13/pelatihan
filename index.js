e48629d5ae029664d0eb4a1cd356ec6d70a50076
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-10-21 21:48:30 +0700

    no message

--- index.js
+++ index.js
@@ -79,11 +79,11 @@ app.use('/api',variasiObatRoute);
  let penilaianTeknisRoute = require('./penilaianTeknis/penilaianTeknisRoute.js');
  app.use('/api',penilaianTeknisRoute);
 
-//  let penilaianNonTeknisRoute = require('./penilaianNonTeknis/PenilaianNonTeknisRoute.js');
-//  app.use('/api',penilaianNonTeknisRoute);
+ let penilaianNonTeknisRoute = require('./penilaianNonTeknis/PenilaianNonTeknisRoute.js');
+ app.use('/api',penilaianNonTeknisRoute);
 
-//  let penilaianAkhirRoute = require('./penilaianAkhir/penilaianAkhirRoute.js');
-//  app.use('/api',penilaianAkhirRoute);
+ let penilaianAkhirRoute = require('./penilaianAkhir/penilaianAkhirRoute.js');
+ app.use('/api',penilaianAkhirRoute);
 
 
 
